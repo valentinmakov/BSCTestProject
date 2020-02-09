9e95de8c88d4a8426f767449f4754d94b703b3d6
Valentin Makov <valentin.makov@gmail.com>
2020-02-09 09:25:17 +0300

    Async await for network call

--- src/Thunks/thunk.ts
+++ src/Thunks/thunk.ts
@@ -10,6 +10,16 @@ const getIsListModel = (dataItem: any): boolean =>
     typeof dataItem.title === 'string'
 
 
+const asyncCall = async <T>(url: string, options: RequestInit): Promise<T> => {
+    const response: Response = await fetch(url, options)
+
+    if (options?.method === 'DELETE' && response.status === 204) {
+        return new Promise(resolve => resolve())
+    }
+
+    return response.json()
+}
+
 export const networkCall = <T>(
     id: string,
     options: RequestInit,
@@ -18,55 +28,49 @@ export const networkCall = <T>(
     const url: string = id.length > 0 ? `${baseURL}/${id}` : `${baseURL}`
     dispatch(actions.dataRequestStart())
 
-    fetch(url, options)
-        .then((resp: Response): Promise<T> => {
-            if (options?.method === 'DELETE' && resp.status === 204) {
-                return new Promise(resolve => resolve())
-            } else {
-                return resp.json()
-            }
-        })
-        .then((json: T): void => {
-            // GET, initial data request
-            if (options?.method === 'GET' && Array.isArray(json)) {
-                const isListModel: boolean = json.every((jsonItem: any): boolean => getIsListModel(jsonItem))
+    const responce: Promise<T> = asyncCall(url, options)
 
-                if (isListModel) {
-                    const result: IActivity[] = json.map((jsonItem: any): IActivity =>
-                        ({
-                            id: jsonItem.id,
-                            title: jsonItem.title,
-                        }),
-                    )
-                    dispatch(actions.dataRequestSuccess(result))
+    responce.then((json: T): void => {
+        // GET, initial data request
+        if (options?.method === 'GET' && Array.isArray(json)) {
+            const isListModel: boolean = json.every((jsonItem: any): boolean => getIsListModel(jsonItem))
+
+            if (isListModel) {
+                const result: IActivity[] = json.map((jsonItem: any): IActivity =>
+                    ({
+                        id: jsonItem.id,
+                        title: jsonItem.title,
+                    }),
+                )
+                dispatch(actions.dataRequestSuccess(result))
+            }
+            dispatch(actions.dataRequestFail({message: 'Invalid data format'}))
+        }
+
+        // POST, add activity request
+        if (options?.method === 'POST') {
+            const isListModel: boolean = getIsListModel(json)
+
+            if (isListModel) {
+                const result: IActivity = {
+                    id: (json as any).id,
+                    title: (json as any).title,
                 }
-                dispatch(actions.dataRequestFail({message: 'Invalid data format'}))
+
+                dispatch(actions.addEventRequestSuccess(result))
+                navigationCallback()
             }
+            dispatch(actions.dataRequestFail({message: 'Invalid data format'}))
+        }
 
-            // POST, add activity request
-            if (options?.method === 'POST') {
-                const isListModel: boolean = getIsListModel(json)
-
-                if (isListModel) {
-                    const result: IActivity = {
-                        id: (json as any).id,
-                        title: (json as any).title,
-                    }
-
-                    dispatch(actions.addEventRequestSuccess(result))
-                    navigationCallback()
-                }
-                dispatch(actions.dataRequestFail({message: 'Invalid data format'}))
-            }
-
-            // DELETE, delete activity request
-            if (options?.method === 'DELETE') {
-                dispatch(actions.dataDeleteSuccess(id))
-            }
-        })
-        .catch((error: any): void => {
-            dispatch(actions.dataRequestFail(error))
-        })
+        // DELETE, delete activity request
+        if (options?.method === 'DELETE') {
+            dispatch(actions.dataDeleteSuccess(id))
+        }
+    })
+    .catch((error: any): void => {
+        dispatch(actions.dataRequestFail(error))
+    })
 }
 
 export const performChangeLanguage = (language: LanguageType): ((dispatch: Function) => void) => (dispatch: Function): void => {
